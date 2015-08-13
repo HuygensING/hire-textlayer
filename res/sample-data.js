bdf20a082786bd48fc9e1482dd30a651dd9b9499
Rene van der Ark <r.van.der.ark@gmail.com>
2015-08-13 13:40:08 +0200

    changed sample-data

--- res/sample-data.js
+++ res/sample-data.js
@@ -1,12 +1,12 @@
 export default {
     "annotationData": [
         {
-            "annotatedText": "Jan van Aken",
-            "n": 9074306,
-            "text": "<div>Aken, Johannes (Jan) Thomasz. van.</div>",
+            "annotatedText": "Person A",
+            "n": 1,
+            "text": "<div>Person A</div>",
             "type": {
-                "description": "Name of person, if possible with PE-number in BIA",
-                "id": 31,
+                "description": "Name of person",
+                "id": 1,
                 "metadata": {
                     "person id": ""
                 },
@@ -14,12 +14,12 @@ export default {
             }
         },
         {
-            "annotatedText": "Katelijn",
-            "n": 9074307,
-            "text": "Aken, Katherina (Katelijn) van.",
+            "annotatedText": "Person B",
+            "n": 2,
+            "text": "Person B",
             "type": {
-                "description": "Name of person, if possible with PE-number in BIA",
-                "id": 31,
+                "description": "Name of person",
+                "id": 1,
                 "metadata": {
                     "person id": ""
                 },
@@ -28,16 +28,16 @@ export default {
         },
         {
             "annotatedText": "",
-            "n": 123456789,
-            "text": "21741",
+            "n": 3,
+            "text": "123456",
             "type": {
                 "description": "",
-                "id": 274,
+                "id": 1,
                 "metadata": {},
                 "name": "elab4:entrylink"
             }
         }
 
     ],
-    "text": "<b>This is received by those who have become brothers and sisters in the aforementioned year.</b><br><br><span style=\"line-height: 2.25em;\">[...]</span><br><span style=\"line-height: 2.25em;\">Item, from master <span data-id=\"9074306\" data-marker=\"begin\" data-param-person_id=\"\" data-type=\"Person\"></span>Jan van Aken<sup data-id=\"9074306\" data-marker=\"end\">1</sup> and <span data-id=\"9074307\" data-marker=\"begin\" data-param-person_id=\"\" data-type=\"Person\"></span>Katelijn<sup data-id=\"9074307\" data-marker=\"end\">2</sup> his wife, 2 pounds.</span><br>[...]"
+    "text": "<b>Lorem ipsum</b><br><br><span style=\"line-height: 2.25em;\">[...]</span><br><span style=\"line-height: 2.25em;\">Some text <span data-id=\"1\" data-marker=\"begin\" data-param-person_id=\"\" data-type=\"Person\"></span>Person A<sup data-id=\"1\" data-marker=\"end\">1</sup> word <span data-id=\"2\" data-marker=\"begin\" data-param-person_id=\"\" data-type=\"Person\"></span>Person B<sup data-id=\"2\" data-marker=\"end\">2</sup> some text</span><br>[...]"
 };
